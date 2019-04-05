51ef889bca728f019b05118680866b41765a1f06
tjforeman <tylerforeman92@gmail.com>
2019-04-05 12:33:44 -0400

    routing added

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import {Route,NavLink} from 'react-router-dom'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -41,8 +42,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm newSmurf={this.newSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+      <div className='navigation'>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/smurf-form'>Add a Smurf</NavLink>
+      </div>
+        <Route path ='/smurf-form' render={props=><SmurfForm {...props}newSmurf={this.newSmurf}/>} />
+    <Route exact path='/' render={props=>  <Smurfs {...props} smurfs={this.state.smurfs} />} />
       </div>
     );
   }
